7e3dadf5bba5bf460d3a785e826dccce76834c0d
leakey1905 <leakey1905@users.noreply.github.com>
2016-11-05 11:12:38 +0800

    Update BlackFriday.c

--- hw100/BlackFriday.c
+++ hw100/BlackFriday.c
@@ -1,18 +1,32 @@
-#include <stdio.h>
+#include<stdio.h>
 
-#define day 13  //get date 13 for each month
-#define Friday 5  //set date to calculate
+#define Friday 5
+
+int isLeapYear(int year){  // use this function to get whether a year is leap year
+	if((year % 4 == 0 && year % 100 != 0) ||(year % 400 == 0 && year % 100 == 0))
+	// leap year can be divided by 4 but not 100 or by 100 and 400 simultaneously
+		return 1;
+	else
+		return 0;
+}
+
 
 int main(){
-	int y, c, m, w;  //year, century, month, weekdate
-	int cnt = 0;  //count how many Friday on 13th in each month
-	scanf("%d", &y);  
-	c = y / 100;
-	y -= c * 100; 
-	for (m = 1; m <= 12; m++){
-		w = y + y / 4 + c / 4 - 2 * c + 26 * (m + 1) / 10 + day - 1; //ref: Zeller formular 
-		if (w % 7 == Friday)
+	int curyear, dayinmonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31}, totalday = 0;
+	int i, day = 1, cnt = 0;  // day Mon - Sun
+	scanf("%d", &curyear);
+	for (i = 1998; i < curyear; i++)
+		if (isLeapYear(i))
+			totalday += 366; // get the day before current year
+		else
+			totalday += 365;
+	if(isLeapYear(curyear))  // check whether current year is leap year
+		dayinmonth[1] = 29;
+	day = (4 + totalday + 13 - 1) % 7;  // get what day is currentyear/01/13:0 == Sun
+	for(i = 0; i < 11; i++){
+		if(day == Friday)
 			cnt++;
+		day = (day + dayinmonth[i]) % 7;	
 	}
 	printf("%d", cnt);
 	return 0;
