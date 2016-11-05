484412f3fe12b45e3ed050d2c628d9c6b7cb764f
leakey1905 <leakey1905@users.noreply.github.com>
2016-11-05 09:52:05 +0800

    Create BlackFriday.c

---
+++ hw100/BlackFriday.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define day 13  //get date 13 for each month
+
+int main(){
+	int y, c, m, w;  //year, century, month, weekdate
+	int cnt = 0;  //count how many Friday on 13th in each month
+	scanf("%d", &y);  
+	c = y / 100;
+	y -= c * 100; 
+	for (m = 1; m <= 12; m++){
+		w = y + y / 4 + c / 4 - 2 * c + 26 * (m + 1) / 10 + day - 1; //ref: Zeller formular 
+		if (w % 7 == 5)
+			cnt++;
+	}
+	printf("%d", cnt);
+	return 0;
+}
