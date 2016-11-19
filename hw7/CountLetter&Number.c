0770b11b5c3afdc8664a02b03bb93d7f3fb4b581
leakey1905 <leakey1905@users.noreply.github.com>
2016-11-19 12:07:53 +0800

    Update CountLetter&Number.c
    Another solution: just count the e f g h i j k in the string.

--- hw7/CountLetter&Number.c
+++ hw7/CountLetter&Number.c
@@ -1,39 +1,16 @@
-//ref: assignment of 范弘毅
-#include<stdio.h>
-
-#define MaxLen 100 //the max length of input string
+#include "stdio.h"
 
 int main(){
-	char s[MaxLen], *pe, *pk, *pl, *pn; //pointer to e, k, letter number
-	int nl=0, nn=0, f=0; //nl number of letter, nn number of number, f flag of exist of e&k
-	pn = pe = gets_s(s, MaxLen);
-	while (*pe != '\0'){
-		if(*pe == 'e' || *pe =='E'){
-			for(pk = s + MaxLen; pk != pe; pk--){
-				if(*pk == 'k'||*pk == 'K'){
-					f = 1;
-					for(pl = pe + 1; pl != pk; pl++){
-						if((*pl >= 'A' && *pl <= 'Z')||(*pl >= 'a' && *pl <= 'z'))
-							nl++;
-					}
-					printf("Found e & k, there are %d letters between them.\n", nl);
-					break;
-				}
-			}
-			break;
-		}
-		pe++;
+	char x[100], *px = x;
+	int i, cntnum = 0, cntchr = 0;
+	printf("请输入一个字符串，长度小于100。\n");
+	scanf("%s", x);
+	for (i = 0; i < 100; i++){
+		if (*(px + i) >= 48 && *(px + i) <= 57)
+			cntnum++;
+		else if (*(px + i) >= 101 && *(px + i) <= 107)
+			cntchr++;
 	}
-	if(f==0)
-		printf("No matched e & k\n");
-
-	while (*pn != '\0'){
-		if(*pn >= '0' && *pn <= '9')
-			nn++;
-		pn++;
-	}
-	printf("Count of number in string is %d.\n",nn);
-	printf("\nP.S. Count the first e and last k.\n");
-	printf("    Do not count the k before e. Do not count the upper and lower letter.\n");
+	printf("结果是数字%d个，e~k字母%d个。\n", cntnum, cntchr);
 	return 0;
 }
